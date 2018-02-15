1246e0e006318636026f80c80aae47435a1828e9
BlanchonMarc <fr.marc.blanchon@gmail.com>
2018-02-15 16:55:39 +0100

    Addition of Custom Log

---
+++ utils/tracker.py
@@ -0,0 +1,26 @@
+"""Custom Log Object - Tracker
+
+This module open, append and close a log file.
+"""
+import datetime
+
+class Tracker:
+    def __init__(self, name : str = 'track.log') -> None:
+        '''Initialize the tracker, open file ```name```'''
+        self.name = name
+        self.file = open(self.name,'w')
+        self.initialized = False
+
+    def __call__(self, _in : str) -> None:
+        '''Append to the self.file'''
+        if not self.initialized:
+            # in case of first opening
+            self.file.write('Tracker Initialized - ' + str(
+                datetime.date.today()) + '\n')
+            self.initialized = not self.initialized
+
+        self.file.write(_in + '\n')
+
+    def _close(self) -> None:
+        '''Close the self.file'''
+        self.file.close()
