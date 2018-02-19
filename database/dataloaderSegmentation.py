22296b29d8f59281e7e83cbe136f3a8059104be4
BlanchonMarc <fr.marc.blanchon@gmail.com>
2018-02-19 09:43:31 +0100

    Changes asked in Pull Request

--- database/dataloaderSegmentation.py
+++ database/dataloaderSegmentation.py
@@ -1,52 +1,78 @@
-
 '''
 Creating a Dataloader for segmentation taks
 '''
 
-import numpy as np
 import os
+import glob
 from PIL import Image
 from torch.utils.data import Dataset
 
 
-class DatasetLoaderSegmentation(Dataset):
-    ''' Derived class to manage Segmentation Dataset Image Loading
-    '''
+class ImageFolderSegmentation(Dataset):
+    """
+        A generic data loader for image segmentation where the data
+        are organised as:
+
+        ....
+
+        Parameters
+        ----------
+        root : str
+            Root folder containing the segmentation data.
+        images : str, default='images'
+            Name of the folder containing input images_root
+        imagext : str, default='png'
+            Extensions of the images contained in the images folder
+        imageconv : str, default='RGB'
+            Conversion color space for inputs
+        labels : str, default='images'
+            Name of the folder containing input labels_root
+        labelsext : str, default='png'
+            Extensions of the images contained in the labels folder
+        labelsconv : str, default='P'
+            Conversion color space for labels
+
+        Attributes
+        ----------
+        images_root : List[str]
+            Images names with full path
+        labels_root : List[str]
+            Labels names with full path
+        ext : List[str]
+            Extansion List
+        conv: List[str]
+            Conversion List
+    """
 
     def __init__(self, root,
                  images='images', imagext='png', imageconv='RGB',
-                 targets='targets', targetext='png', targetconv='P'):
-        # parameters copies
-        self.images_root = os.path.join(root, images)
-        self.labels_root = os.path.join(root, targets)
-        self.ext = [imagext, targetext]
+                 labels='labels', labelsext='png', labelsconv='P'):
+        images_root = os.path.join(root, images)
+        labels_root = os.path.join(root, labels)
 
-        # Extract the filenames without extansions
-        self.filenames = [os.path.basename(os.path.splitext(fname)[0])
-                          for fname in os.listdir(self.labels_root)
-                          if any(filename.endswith(ext) for ext in self.ext)]
+        self.image_filenames = sorted(glob.glob(self.images_root))
+        self.label_filenames = sorted(glob.glob(self.labels_root))
 
-        # Alphabetical Order
-        self.filenames.sort()
+        self.ext = [imagext, labelsext]
+        self.conv = [imageconv, labelsconv]
+
+        if not all([_get_filename(imf) == _get_filename(lf)
+                    for imf, lf in zip(image_filenames, label_filenames)]):
+                raise ValueError('''Image names in Images and
+                                 labelss have to be identical''')
+
+    def _get_filename(path):
+        return os.path.basename(os.path.splitext(path)[0])
 
     def __getitem__(self, index):
-        '''Convert the images and return to be passed in dataloader
+        '''Get an image and a label'''
+        with open(image_filenames[index], 'rb') as f:
+            image = Image.open(f).convert(self.conv[0])
 
-        Dataloader will recieve the images and transform
-        It will also allow the paralellization / optimization
-        '''
+        with open(label_filenames[index], 'rb') as f:
+            labels = Image.open(f).convert(self.conv[1])
 
-        # Images
-        with open(os.path.join(self.images_root, self.filenames[index] \
-                               + self.ext[0]), 'rb') as f:
-            image = Image.open(f).convert(imageconv)
-
-        # Targets
-        with open(os.path.join(self.labels_root, self.filenames[index] \
-                               + self.ext[1]), 'rb') as f:
-            target = Image.open(f).convert(targetconv)
-
-        return image, target
+        return image, labels
 
     def __len__(self):
-        return len(self.filenames)
+        return len(self.image_filenames)
