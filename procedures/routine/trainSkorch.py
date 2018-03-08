7993a91a6c9eee529f6eb8a5651f1147712d23b1
BlanchonMarc <fr.marc.blanchon@gmail.com>
2018-03-08 11:24:56 +0100

    skorch server

--- procedures/routine/trainSkorch.py
+++ procedures/routine/trainSkorch.py
@@ -28,10 +28,8 @@ from multiprocessing import set_start_method
 from sklearn.grid_search import GridSearchCV
 
 
-try:
-    set_start_method('spawn')
-except RuntimeError:
-    pass
+set_start_method('spawn')
+
 
 
 class NormalizeInput:
@@ -164,10 +162,10 @@ params = {
 image_indicators = np.hstack([np.repeat(i, len(x)) for i, x in
                              enumerate(X)])
 labels = image_indicators % n_classes
-X, y = np.vstack(X), np.hstack(Y)
+#X, y = np.vstack(X), np.hstack(Y)
 
 cv = LeavePLabelOut(labels=labels, p=1)
 gs = GridSearchCV(net, params, scoring='f1', verbose=10, cv=cv, n_jobs=-1)
 
-gs.fit(X=X, y=y)
+gs.fit(X=X, y=Y)
 print(gs.best_score_, gs.best_params_)
