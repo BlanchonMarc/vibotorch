57f888862408eb1264320292e687423462208bc5
BlanchonMarc <fr.marc.blanchon@gmail.com>
2018-03-08 09:35:50 +0100

    Testing Skorch on server

--- procedures/routine/trainSkorch.py
+++ procedures/routine/trainSkorch.py
@@ -24,7 +24,7 @@ from skorch import NeuralNet
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.cross_validation import LeavePLabelOut
-from multiprocessing import set_start_methodfrom
+from multiprocessing import set_start_method
 from sklearn.grid_search import GridSearchCV
 
 
