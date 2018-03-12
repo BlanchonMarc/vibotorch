aa553cf3993851225385e1b4d98967cd109cca09
BlanchonMarc <fr.marc.blanchon@gmail.com>
2018-03-12 10:34:19 +0100

    train modified

--- procedures/routine/trainSkorch.py
+++ procedures/routine/trainSkorch.py
@@ -162,7 +162,8 @@ params = {
 image_indicators = np.hstack([np.repeat(i, len(x)) for i, x in
                              enumerate(X)])
 labels = image_indicators % n_classes
-#X, y = np.vstack(X), np.hstack(Y)
+#
+X, y = np.vstack(X), np.hstack(Y)
 
 cv = LeavePLabelOut(labels=labels, p=1)
 gs = GridSearchCV(net, params, scoring='f1', verbose=10, cv=cv, n_jobs=-1)
