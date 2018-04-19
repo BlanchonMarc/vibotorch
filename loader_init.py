076f508fc5391e24e50a1c713f9157be93c94bff
BlanchonMarc <fr.marc.blanchon@gmail.com>
2018-04-19 15:22:21 +0200

    .

--- loader_init.py
+++ loader_init.py
@@ -33,11 +33,13 @@ def loader_init(image_path, label_path, image_path2, label_path2,
                 batch_size, num_workers):
 
     transform = Compose([
+        CenterCrop(256),
         ToTensor(),
         # NormalizeInput(),
         Normalize([0.3120, 0.3018, 0.2944], [0.2169, 0.2097, 0.2087]),
     ])
     label_transform = Compose([
+        CenterCrop(256),
         load_label(),
     ])
 
